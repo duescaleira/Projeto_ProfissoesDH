e62f4054cc29814fdad6e98b16d45d2615d0e393
Edu Escaleira <duescaleira@gmail.com>
2021-09-24 21:32:02 -0300

    first commit

---
+++ Projeto_ProfissoesDH/Projeto_ProfissoesDH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Projeto_ProfissoesDH
+//
+//  Created by Eduardo Escaleira on 24/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Projeto_ProfissoesDH/Projeto_ProfissoesDHUITests/Projeto_ProfissoesDHUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Projeto_ProfissoesDHUITestsLaunchTests.swift
+//  Projeto_ProfissoesDHUITests
+//
+//  Created by Eduardo Escaleira on 24/09/21.
+//
+
+import XCTest
+
+class Projeto_ProfissoesDHUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
