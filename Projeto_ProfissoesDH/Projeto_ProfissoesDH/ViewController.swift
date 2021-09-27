0bba86cdeccef85968de43f0e690a9bbc9e6005a
Edu Escaleira <duescaleira@gmail.com>
2021-09-27 10:47:49 -0300

    - Adicionada pasta "feature" onde cada classe deve ser salva em um arquivo diferente - IBoulets & Action adicionados

--- Projeto_ProfissoesDH/Projeto_ProfissoesDH/ViewController.swift
+++ Projeto_ProfissoesDH/Projeto_ProfissoesDH/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var labelNames: UILabel!
+    @IBOutlet var labelProfission: UILabel!
+    
+    @IBOutlet var labelNameOne: UILabel!
+    @IBOutlet var labelNameTwo: UILabel!
+    @IBOutlet var labelProfissionOne: UILabel!
+    @IBOutlet var labelProfissionTwo: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func confirmButton(_ sender: UIButton) {
+    }
 }
 
