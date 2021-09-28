6f2bb33a4101d818cbb9e278eecd035dbac437bf
Edu Escaleira <duescaleira@gmail.com>
2021-09-28 08:14:17 -0300

    App finalizado

---
+++ Projeto_ProfissoesDH/Projeto_ProfissoesDH/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  Projeto_ProfissoesDH
+//
+//  Created by Eduardo Escaleira on 28/09/21.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- Projeto_ProfissoesDH/Projeto_ProfissoesDH/Users.swift
+++ Projeto_ProfissoesDH/Projeto_ProfissoesDH/Users.swift
@@ -20,7 +20,13 @@ let profissionais = [
     Funcionario(profissao: "Professor", especialidade: "Matemática", nome: "Adriano"), // 4
     Funcionario(profissao: "Professor", especialidade: "Física", nome: "Roberto"), // 5
     Funcionario(profissao: "Engenheiro", especialidade: "Elétrico", nome: "Victor"), // 6
-    Funcionario(profissao: "Engenheiro", especialidade: "Civil", nome: "Sérgio") // 7
+    Funcionario(profissao: "Engenheiro", especialidade: "Civil", nome: "Sérgio"), // 7
+    Funcionario(profissao: "Biólogo", especialidade: "Marinho", nome: "Ícaro"),
+    Funcionario(profissao: "Biólogo", especialidade: "Aves", nome: "Darwin"),
+    Funcionario(profissao: "Fisioterapeuta", especialidade: "Infantil", nome: "André"),
+    Funcionario(profissao: "Fisioterapeuta", especialidade: "Respiratório", nome: "José"),
+    Funcionario(profissao: "Químico", especialidade: "Ácidos", nome: "Caio"),
+    Funcionario(profissao: "Químico", especialidade: "Eletrons", nome: "Fernando")
 ]
 
     private func randomNumber() {
@@ -34,99 +40,51 @@ let profissionais = [
         
         n1 = randomNum
         n2 = randomNumTwo
-        print("n1:\(n1) n2:\(n2)")
+//        print("n1:\(n1) n2:\(n2)")
     }
     
     func getNome() -> String {
          randomNumber()
-         return profissionais[n1].nome
+         return "Nome: \(profissionais[n1].nome)"
    
     }
     
+    func randomProf()  {
+        
+        var profUm = profissionais[n1].profissao
+        var profDois = profissionais[n2].profissao
+        
+        while profUm == profDois {
+            randomNumber()
+            profUm = profissionais[n1].profissao
+            profDois = profissionais[n2].profissao
+            break
+        }
+    }
+    
     func getProfissional() -> String {
-        return profissionais[n1].profissao
+        randomProf()
+//        print(profissionais[n1].profissao)
+        return "Profissão: \(profissionais[n1].profissao)"
         
     }
     
     func getEspecialidade() -> String {
-        return profissionais[n1].especialidade
+        return "Espec.: \(profissionais[n1].especialidade)"
     }
     
-//    private func randomNumberTwo() -> Int  {
-//        let randomNumTwo = Int.random(in: 0..<profissionais.count)
-//        n2 = randomNumTwo
-//        print(n2)
-//        return n2
-//    }
-    
     func getNomeDois() -> String {
-        let nom2 = profissionais[n2].nome
-        return nom2
+        return "Nome: \(profissionais[n2].nome)"
 
     }
     
     func getProfissionalDois() -> String {
-        return profissionais[n2].profissao
+        randomProf()
+//        print(profissionais[n2].profissao)
+        return "Profissão: \(profissionais[n2].profissao)"
     }
     
     func getEspecialidadeDois() -> String {
-        return profissionais[n2].especialidade
+        return "Espec.: \(profissionais[n2].especialidade)"
     }
 }
-    
-//    private func randomNumberTwo() -> Int  {
-//        let randomNumTwo = Int.random(in: 0..<profissionais.count)
-//        n2 = randomNumTwo
-//
-//        print(n2)
-//        return n2
-//    }
-//
-//
-//    func getProfissionaisIguais() -> Bool {
-//        if profissionais[n1].profissao == profissionais[n2].profissao  {
-//            while profissionais[n1].profissao == profissionais[n2].profissao {
-//                n1 = randomNumber()
-//                n2 = randomNumberTwo()
-//            }
-//    }
-//
-//
-//    func getRandomNome() -> String {
-//        return profissionais[randomNumber()].nome
-//    }
-//
-//    func getRandomNomeDois() -> String {
-//        return profissionais[randomNumberTwo()].nome
-//    }
-    
-//    func getRandomEspecialidade() -> String {
-//        if getProfissionaisIguais() == true {
-//            return profissionais[n1].especialidade
-//        }
-//        else {
-//        return ""
-//        }
-//    }
-//
-//
-//    func getRandomNomeDois() -> String {
-//        if getProfissionaisIguais() == true {
-//            return profissionais[randomNumberTwo()].nome
-//        }
-//        else {
-//        return ""
-//        }
-//    }
-//
-//    func getRandomEspecialidadeDois() -> String {
-//        if getProfissionaisIguais() == true {
-//            return profissionais[n2].especialidade
-//        }
-//        else {
-//        return ""
-//        }
-//    }
-    
-
-
